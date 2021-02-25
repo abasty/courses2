4a0ad1398ec571b0997c5977f0b948b2a36bca55
Alain Basty <alain.basty@free.fr>
2021-02-25 07:12:43 +0100

    First working widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,19 +5,41 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-// import 'package:courses2/modele.dart';
-// import 'package:courses2/liste_screen.dart';
+import 'package:courses2/modele.dart';
+import 'package:courses2/liste_screen.dart';
 
-void main() {
-  testWidgets('Courses II Test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    //await tester.pumpWidget(ListeScreen());
+import 'dataset.dart';
 
-    //expect(find.text('Produits'), findsOneWidget);
-    //await tester.tap(find.byIcon(Icons.add));
-    //await tester.pump();
+void main() async {
+  modele = ModeleCourses(DatasetStorageCourses());
+  modele.readAll();
+  await modele.isLoaded;
+
+  testWidgets('ListeScreen Widget Test', (WidgetTester tester) async {
+    await tester.runAsync(() async {
+      // test code here
+      await tester.pumpWidget(
+        // Wrap our widget in a MaterialApp for MediaQuery
+        MaterialApp(home: ListeScreen()),
+      );
+
+      // First frame just display the progress indicator
+      var progress = find.byType(CircularProgressIndicator);
+      print(progress);
+
+      // Take a frame for the FutureBuilder to realize
+      await tester.pump();
+
+      // The progress indicator went away and the list is displayed
+      progress = find.byType(CircularProgressIndicator);
+      print(progress);
+      final text = find.text('Produits');
+      print(text);
+      final icon = find.byIcon(Icons.add_circle);
+      print(icon);
+    });
   });
 }
