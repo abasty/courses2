d90e918f16aef23626d9fe6e7ebc46482a1b4030
Alain Basty <alain.basty@free.fr>
2021-03-29 08:09:27 +0200

    Cleaning: renamed map to str

--- lib/storage.dart
+++ lib/storage.dart
@@ -20,9 +20,9 @@ class LocalStorageStrategy extends StorageStrategy {
   @override
   Future<Map<String, dynamic>> readAll() async {
     await _storage.ready;
-    var map = await _storage.getItem('modele') as String?;
-    map ??= await rootBundle.loadString('assets/courses.json');
+    var str = await _storage.getItem('modele') as String?;
+    str ??= await rootBundle.loadString('assets/courses.json');
     await Future.delayed(Duration(seconds: 2));
-    return json.decode(map) as Map<String, dynamic>;
+    return json.decode(str) as Map<String, dynamic>;
   }
 }
