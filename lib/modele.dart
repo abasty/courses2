b6c40437a06feb35f50eef144a76109460c0611c
Alain Basty <alain.basty@free.fr>
2021-04-25 22:50:25 +0200

    Fixed update

--- lib/modele.dart
+++ lib/modele.dart
@@ -4,27 +4,10 @@ library modele;
 
 import 'package:courses3/backend.dart';
 import 'package:flutter/foundation.dart';
+
 import 'storage.dart';
 
-/// Un rayon défini par son [nom].
-class Rayon {
-  /// Le nom de ce [Rayon].
-  String nom;
-
-  /// Crée un nouveau [Rayon] avec son [nom].
-  Rayon(this.nom);
-
-  /// Crée un nouveau [Rayon] depuis une [map].
-  factory Rayon.fromMap(Map<String, dynamic> map) =>
-      Rayon(map['nom'] as String);
-
-  /// Transforme ce [Rayon] en `Map<String, dynamic>`.
-  Map<String, dynamic> toMap() => {'nom': nom};
-
-  /// Renvoie une représentation textuelle de ce [Rayon].
-  @override
-  String toString() => 'Rayon(nom: $nom)';
-}
+late VueModele modele;
 
 /// Un produit défini par son [nom] et son [rayon].
 class Produit extends ChangeNotifier {
@@ -61,36 +44,37 @@ class Produit extends ChangeNotifier {
       'Produit(nom: $nom, rayon: $rayon, quantite: $quantite, fait: $fait)';
 }
 
+/// Un rayon défini par son [nom].
+class Rayon {
+  /// Le nom de ce [Rayon].
+  String nom;
+
+  /// Crée un nouveau [Rayon] avec son [nom].
+  Rayon(this.nom);
+
+  /// Crée un nouveau [Rayon] depuis une [map].
+  factory Rayon.fromMap(Map<String, dynamic> map) =>
+      Rayon(map['nom'] as String);
+
+  /// Transforme ce [Rayon] en `Map<String, dynamic>`.
+  Map<String, dynamic> toMap() => {'nom': nom};
+
+  /// Renvoie une représentation textuelle de ce [Rayon].
+  @override
+  String toString() => 'Rayon(nom: $nom)';
+}
+
 /// Le vue modèle et son contrôleur.
 class VueModele extends ChangeNotifier {
   final StorageStrategy _storage;
 
   late Future<void> _isLoaded;
 
-  /// [isLoaded] se réalise quand le [loadAll()] initial est terminé.
-  Future<void> get isLoaded => _isLoaded;
-
-  /// [isConnected] est [true] si le _storage_ est connecté
-  bool get isConnected => _storage.isConnected;
-
   final Rayon _divers = Rayon('Divers');
-
-  /// Le [Rayon] 'Divers'.
-  Rayon get divers => _divers;
 
   final List<Rayon> _rayons = [];
 
-  /// La liste des rayons.
-  List<Rayon> get rayons => _rayons;
-
   final List<Produit> _produits = [];
-
-  /// La liste des produits.
-  List<Produit> get produits => _produits;
-
-  /// La liste des produits avec des quantités > 0.
-  List<Produit> get selection =>
-      _produits.where((p) => p.quantite > 0).toList();
 
   /// Crée le modèle et charge les données suivant la [StorageStrategy] en
   /// paramètre.
@@ -100,27 +84,50 @@ class VueModele extends ChangeNotifier {
     if (_storage is BackendStrategy) _storage.pushEvent = _pushCallback;
   }
 
-  /// Notifie la vue, sauve en local et pousse le produit sur le serveur
-  void _changeProduit(Produit p, [Map<String, String>? aux]) {
-    p.notifyListeners();
-    saveAll();
-    _advertise(p, aux);
+  /// Le [Rayon] 'Divers'.
+  Rayon get divers => _divers;
+
+  /// [isConnected] est [true] si le _storage_ est connecté
+  bool get isConnected => _storage.isConnected;
+
+  /// [isLoaded] se réalise quand le [loadAll()] initial est terminé.
+  Future<void> get isLoaded => _isLoaded;
+
+  /// La liste des produits.
+  List<Produit> get produits => _produits;
+
+  /// La liste des rayons.
+  List<Rayon> get rayons => _rayons;
+
+  /// La liste des produits avec des quantités > 0.
+  List<Produit> get selection =>
+      _produits.where((p) => p.quantite > 0).toList();
+
+  /// Déconnexion du storage
+  void ctrlDeconnexion() {
+    _storage.disconnect();
+    notifyListeners();
   }
 
-  void _advertise(Produit p, [Map<String, String>? aux]) async {
-    if (isConnected) {
-      var map = p.toMap();
-      if (aux != null) map.addAll(aux);
-      await _storage.advertise('courses/produit', map);
-      if (!isConnected) notifyListeners();
+  /// Met à jour ou ajoute un produit.
+  void ctrlMajProduit(Produit? p, Produit maj) {
+    Map<String, String>? replace;
+    if (p == null) {
+      p = _addSingleProduit(maj);
+    } else {
+      if (p.nom != maj.nom && !_majNomPossible(maj.nom)) return;
+      replace = {'update': p.nom};
+      p.nom = maj.nom;
+      p.rayon = maj.rayon;
     }
+    _sort();
+    _changeProduit(p, replace);
+    notifyListeners();
   }
 
-  /// Incrémente la quantité du [Produit] [p].
-  void ctrlProduitPlus(Produit p) {
-    p.quantite++;
-    p.fait = false;
-    _changeProduit(p);
+  /// Définit la quantite du [Produit] [p] à 0 ou 1.
+  void ctrlProduitInverse(Produit p) {
+    p.quantite == 0 ? modele.ctrlProduitPlus(p) : modele.ctrlProduitRaz(p);
   }
 
   /// Décrémente la quantite du [Produit] [p].
@@ -131,22 +138,24 @@ class VueModele extends ChangeNotifier {
     _changeProduit(p);
   }
 
-  /// Définit la quantite du [Produit] [p] à 0.
-  void ctrlProduitRaz(Produit p) {
-    if (p.quantite == 0) return;
-    p.quantite = 0;
+  /// Incrémente la quantité du [Produit] [p].
+  void ctrlProduitPlus(Produit p) {
+    p.quantite++;
     p.fait = false;
     _changeProduit(p);
-  }
-
-  /// Définit la quantite du [Produit] [p] à 0 ou 1.
-  void ctrlProduitInverse(Produit p) {
-    p.quantite == 0 ? modele.ctrlProduitPlus(p) : modele.ctrlProduitRaz(p);
   }
 
   /// Marque ou démarque le [Produit] [p].
   void ctrlProduitPrend(Produit p, bool value) {
     p.fait = value;
+    _changeProduit(p);
+  }
+
+  /// Définit la quantite du [Produit] [p] à 0.
+  void ctrlProduitRaz(Produit p) {
+    if (p.quantite == 0) return;
+    p.quantite = 0;
+    p.fait = false;
     _changeProduit(p);
   }
 
@@ -165,37 +174,36 @@ class VueModele extends ChangeNotifier {
     saveAll();
   }
 
-  /// Met à jour ou ajoute un produit.
-  void ctrlMajProduit(Produit? p, Produit maj) {
-    Map<String, String>? replace;
-    if (p == null) {
-      p = _addSingleProduit(maj);
-    } else {
-      replace = {'update': p.nom};
-      p.nom = maj.nom;
-      p.rayon = maj.rayon;
-    }
-    _produits.sort((a, b) => a.rayon.nom.compareTo(b.rayon.nom));
-    _changeProduit(p, replace);
-    notifyListeners();
+  /// Importe une liste de [Produit] et une liste de [Rayon] depuis une [map]
+  /// dans ce [VueModele]. Les rayons et produits seront uniques par rapport à
+  /// leur nom. Les listes sont triées par ordre alphabétique des noms. La
+  /// sélection est mise à jour.
+  void importFromMap(Map<String, dynamic> map) {
+    _rayons.add(_divers);
+    (map['rayons'] as List).forEach((r) => _addSingleRayon(r['nom'] as String));
+    (map['produits'] as List).forEach(
+        (p) => _addSingleProduit(Produit.fromMap(p as Map<String, dynamic>)));
+    _sort();
   }
 
-  /// Déconnexion du storage
-  void ctrlDeconnexion() {
-    _storage.disconnect();
-    notifyListeners();
-  }
-
-  Rayon _addSingleRayon(String nom) {
-    Rayon rayon;
+  /// Charge dans le modèle toutes les données du stockage.
+  Future<void> loadAll() async {
     try {
-      rayon = _rayons.singleWhere((r) => r.nom == nom);
-    } on StateError {
-      rayon = Rayon(nom);
-      _rayons.add(rayon);
+      importFromMap(await _storage.read());
+    } on Error {
+      debugPrint('Erreur de lecture. Fallback sur les données intégrées.');
+      importFromMap(await readFromAsset('courses'));
     }
-    return rayon;
   }
+
+  /// Sauve les données du modèle sur le sockage.
+  Future<void> saveAll() async => await _storage.write(toMap());
+
+  /// Transforme ce [VueModele] en `Map<String, dynamic>`
+  Map<String, dynamic> toMap() => {
+        'rayons': _rayons.map((rayon) => rayon.toMap()).toList(),
+        'produits': _produits.map((produit) => produit.toMap()).toList(),
+      };
 
   Produit _addSingleProduit(Produit produit) {
     var rayon = _addSingleRayon(produit.rayon.nom);
@@ -215,56 +223,55 @@ class VueModele extends ChangeNotifier {
     }
   }
 
+  Rayon _addSingleRayon(String nom) {
+    Rayon rayon;
+    try {
+      rayon = _rayons.singleWhere((r) => r.nom == nom);
+    } on StateError {
+      rayon = Rayon(nom);
+      _rayons.add(rayon);
+    }
+    return rayon;
+  }
+
   // Callback de push SSE
-  void _pushCallback(Map<String, dynamic> map) {
-    var produitNom = map['update'] ?? map['nom'];
-    if (produitNom == null) return;
-    var nouveau = Produit.fromMap(map);
-    var existant = _produits.singleWhere((p) => p.nom == produitNom,
-        orElse: () => nouveau);
-    existant.nom = nouveau.nom;
-    var sel_change = nouveau.quantite != existant.quantite &&
-        (nouveau.quantite == 0 || existant.quantite == 0);
-    var produit = _addSingleProduit(nouveau);
-    // Si le produit n'existait pas ou sa selection a changé
-    if (produit == nouveau || sel_change) {
-      notifyListeners();
-    } else {
-      produit.notifyListeners();
+  void _advertise(Produit p, [Map<String, String>? aux]) async {
+    if (isConnected) {
+      var map = p.toMap();
+      if (aux != null) map.addAll(aux);
+      await _storage.advertise('courses/produit', map);
+      if (!isConnected) notifyListeners();
     }
   }
 
-  /// Transforme ce [VueModele] en `Map<String, dynamic>`
-  Map<String, dynamic> toMap() => {
-        'rayons': _rayons.map((rayon) => rayon.toMap()).toList(),
-        'produits': _produits.map((produit) => produit.toMap()).toList(),
-      };
+  /// Notifie la vue, sauve en local et pousse le produit sur le serveur
+  void _changeProduit(Produit p, [Map<String, String>? aux]) {
+    p.notifyListeners();
+    saveAll();
+    _advertise(p, aux);
+  }
 
-  /// Importe une liste de [Produit] et une liste de [Rayon] depuis une [map]
-  /// dans ce [VueModele]. Les rayons et produits seront uniques par rapport à
-  /// leur nom. Les listes sont triées par ordre alphabétique des noms. La
-  /// sélection est mise à jour.
-  void importFromMap(Map<String, dynamic> map) {
-    _rayons.add(_divers);
-    (map['rayons'] as List).forEach((r) => _addSingleRayon(r['nom'] as String));
-    (map['produits'] as List).forEach(
-        (p) => _addSingleProduit(Produit.fromMap(p as Map<String, dynamic>)));
+  bool _majNomPossible(String nom) {
+    return _produits.where((p) => p.nom == nom).isEmpty;
+  }
+
+  void _pushCallback(Map<String, dynamic> map) {
+    var nouveau = Produit.fromMap(map);
+    var ancien_nom = map['update'] as String?;
+    if (ancien_nom != null) {
+      if (!_majNomPossible(nouveau.nom)) return;
+      var existant = _produits.singleWhere((p) => p.nom == ancien_nom,
+          orElse: () => nouveau);
+      if (existant == nouveau) return;
+      existant.nom = nouveau.nom;
+    }
+    _addSingleProduit(nouveau);
+    _sort();
+    notifyListeners();
+  }
+
+  void _sort() {
     _rayons.sort((a, b) => a.nom.compareTo(b.nom));
     _produits.sort((a, b) => a.rayon.nom.compareTo(b.rayon.nom));
   }
-
-  /// Sauve les données du modèle sur le sockage.
-  Future<void> saveAll() async => await _storage.write(toMap());
-
-  /// Charge dans le modèle toutes les données du stockage.
-  Future<void> loadAll() async {
-    try {
-      importFromMap(await _storage.read());
-    } on Error {
-      debugPrint('Erreur de lecture. Fallback sur les données intégrées.');
-      importFromMap(await readFromAsset('courses'));
-    }
-  }
 }
-
-late VueModele modele;
